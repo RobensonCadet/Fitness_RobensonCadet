7cd3c12e94ea40ad16c2e3ba9f5a977ea99b441e
RobensonCadet <robensoncadet@gmail.com>
2020-10-29 23:29:23 -0300

    Initial commit

---
+++ app/src/main/java/Clases/Planes.java
@@ -0,0 +1,29 @@
+package Clases;
+
+public class Planes {
+
+    // Declaración de datos.
+
+    private int Xtreme;
+    private int Mindfullnes;
+
+    // Constructor
+
+    public Planes()
+    {
+        Xtreme = 12000;
+        Mindfullnes = 24000;
+    }
+
+    // Accesadores
+
+    public int getXtreme()
+    {
+        return Xtreme;
+    }
+
+    public int getMindfullnes()
+    {
+        return Mindfullnes;
+    }
+}
